94f9595d55035ed5d0acb111ff25c2e35746376c
Stefan Plösser <stefan@rarelime.de>
2020-02-28 13:58:24 +0100

    Hub mode

--- screenlayout/hub.sh
+++ screenlayout/hub.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x364 --rotate normal --output DP1 --off --output DP2 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --mode 1600x900 --pos 1920x0 --rotate normal
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2-1 --off --output DP2-2 --primary --mode 2560x1440 --pos 0x264 --rotate normal --output DP2-3 --mode 1920x1080 --pos 2648x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off

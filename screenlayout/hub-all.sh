ed6527a87bea9d102bf7fa2e1f302aa700e159eb
Stefan Plösser <stefan@rarelime.de>
2020-03-17 10:17:50 +0100

    hub-all screenlayout

---
+++ screenlayout/hub-all.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x1408 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --primary --mode 2560x1440 --pos 2000x280 --rotate normal --output DP2-3 --mode 1920x1080 --pos 4704x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
